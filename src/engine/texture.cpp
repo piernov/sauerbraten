3f6f21b75cfa199500c9b13470a86d046162fdef
piernov <piernov@piernov.org>
2016-04-15 00:18:46 +0200

    Fix XCode build Fix and update XCode project Replace SDL frameworks with SDL 2.0.4, SDL_image&SDL_mixer 2.0.1 Remove SDL2's include workaround

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -2,11 +2,7 @@
 
 #include "engine.h"
 
-#ifdef __APPLE__
-  #include "SDL2_image/SDL_image.h"
-#else
-  #include "SDL_image.h"
-#endif
+#include "SDL_image.h"
 
 #define FUNCNAME(name) name##1
 #define DEFPIXEL uint OP(r, 0);
