ec476c9c34e2e22956d7dd7906a1032082b00808
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2019-01-29 07:06:31 +0000

    vrotate 6 and 7
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@5387 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/3dgui.cpp
+++ src/engine/3dgui.cpp
@@ -699,9 +699,9 @@ struct gui : g3d_gui
         float xoff = vslot.offset.x, yoff = vslot.offset.y;
         if(vslot.rotation)
         {
-            if((vslot.rotation&5) == 1) { swap(xoff, yoff); loopk(4) swap(tc[k].x, tc[k].y); }
-            if(vslot.rotation >= 2 && vslot.rotation <= 4) { xoff *= -1; loopk(4) tc[k].x *= -1; }
-            if(vslot.rotation <= 2 || vslot.rotation == 5) { yoff *= -1; loopk(4) tc[k].y *= -1; }
+            if((vslot.rotation&5) == 1 || vslot.rotation >= 6) { swap(xoff, yoff); loopk(4) swap(tc[k].x, tc[k].y); }
+            if((vslot.rotation >= 2 && vslot.rotation <= 4) || vslot.rotation == 7) { xoff *= -1; loopk(4) tc[k].x *= -1; }
+            if(vslot.rotation <= 2 || vslot.rotation == 5 || vslot.rotation == 7) { yoff *= -1; loopk(4) tc[k].y *= -1; }
         }
         loopk(4) { tc[k].x = tc[k].x/xt - xoff/t->xs; tc[k].y = tc[k].y/yt - yoff/t->ys; } 
         if(slot.loaded) gle::color(vec(color).mul(vslot.colorscale));
