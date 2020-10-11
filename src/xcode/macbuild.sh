8b84fe082b22c1574737e4049d0430bcb44f6355
qreeves <qreeves@07af1644-1164-4067-b897-63bf4dc9b748>
2020-10-11 18:59:29 +0000

    store macOS app in main folder
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@6196 07af1644-1164-4067-b897-63bf4dc9b748

--- src/xcode/macbuild.sh
+++ src/xcode/macbuild.sh
@@ -8,8 +8,8 @@ while [ -n "$1" ]; do
    xcodebuild -project src/xcode/sauerbraten.xcodeproj -configuration Release -alltargets
    ;;
   install)
-   cp -v src/xcode/build/Release/sauerbraten.app/Contents/MacOS/sauerbraten bin_unix/sauerbraten.app/Contents/MacOS/sauerbraten_universal
-   chmod +x bin_unix/sauerbraten.app/Contents/MacOS/sauerbraten_universal
+   cp -v src/xcode/build/Release/sauerbraten.app/Contents/MacOS/sauerbraten sauerbraten.app/Contents/MacOS/sauerbraten_universal
+   chmod +x sauerbraten.app/Contents/MacOS/sauerbraten_universal
    ;;
  esac
  shift
