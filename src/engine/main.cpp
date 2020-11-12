aa499cf8d38b158841a0487d10f11f6731d14050
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-11-12 05:04:09 +0000

    avoid redundant inputgrabs
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@6235 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -863,7 +863,7 @@ void checkinput()
     {
         if(events.length()) event = events.remove(0);
 
-        if(focused && event.type!=SDL_WINDOWEVENT) { inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
+        if(focused && event.type!=SDL_WINDOWEVENT) { if(grabinput != (focused>0)) inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
 
         switch(event.type)
         {
@@ -965,7 +965,7 @@ void checkinput()
                 break;
         }
     }
-    if(focused) { inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
+    if(focused) { if(grabinput != (focused>0)) inputgrab(grabinput = focused>0, shouldgrab); focused = 0; }
     if(mousemoved) resetmousemotion();
 }
 
