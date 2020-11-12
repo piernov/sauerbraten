0b2e22d5547b68247fdd887b58e77491638532ef
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-11-12 05:28:49 +0000

    avoid extra window grab toggles
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@6236 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -484,8 +484,8 @@ void inputgrab(bool on, bool delay = false)
         SDL_ShowCursor(SDL_TRUE);
         if(relativemouse)
         {
-            SDL_SetRelativeMouseMode(SDL_FALSE);
             SDL_SetWindowGrab(screen, SDL_FALSE);
+            SDL_SetRelativeMouseMode(SDL_FALSE);
             relativemouse = false;
         }
     }
