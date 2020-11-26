e21c3e23551642921ffa7dcfefb2b88f348f4c74
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-11-26 23:13:10 +0000

    add package command to macbuild
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@6359 07af1644-1164-4067-b897-63bf4dc9b748

--- src/xcode/macbuild.sh
+++ src/xcode/macbuild.sh
@@ -12,6 +12,9 @@ while [ -n "$1" ]; do
    cp -v build/Release/sauerbraten.app/Contents/MacOS/sauerbraten ../../sauerbraten.app/Contents/MacOS/sauerbraten_universal
    chmod +x ../sauerbraten.app/Contents/MacOS/sauerbraten_universal
    ;;
+  package)
+   exec package.sh
+   ;;
  esac
  shift
 done
