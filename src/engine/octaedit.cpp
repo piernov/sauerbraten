b74d8dacd4bf9109eac40f4ca9f98dab79258c75
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-12-22 01:08:16 +0000

    passthrough fix
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@6484 07af1644-1164-4067-b897-63bf4dc9b748

--- src/engine/octaedit.cpp
+++ src/engine/octaedit.cpp
@@ -351,6 +351,8 @@ extern float rayent(const vec &o, const vec &ray, float radius, int mode, int si
 
 VAR(gridlookup, 0, 0, 1);
 VAR(passthroughcube, 0, 1, 1);
+VAR(passthroughent, 0, 1, 1);
+VARF(passthrough, 0, 0, 1, { passthroughsel = passthrough; entcancel(); if(passthroughent) setvar("entediting", passthrough ? 0 : 1); });
 
 void rendereditcursor()
 {
@@ -393,7 +395,7 @@ void rendereditcursor()
                        (editmode && showmat ? RAY_EDITMAT : 0)   // select cubes first
                        | (!dragging && entediting ? RAY_ENTS : 0)
                        | RAY_SKIPFIRST
-                       | (passthroughcube==1 ? RAY_PASS : 0), gridsize, entorient, ent);
+                       | (passthroughcube || passthrough ? RAY_PASS : 0), gridsize, entorient, ent);
 
         if((havesel || dragging) && !passthroughsel && !hmapedit)     // now try selecting the selection
             if(rayboxintersect(vec(sel.o), vec(sel.s).mul(sel.grid), player->o, camdir, sdist, orient))
